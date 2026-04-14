d91c4117a94d8a3ebe64e7dd4b85cfcb0460bbd6
Alex Öhman <ohman2303@gmail.com>
2026-04-14 21:35:28 +0300

    addLutemonia alottelin

--- app/src/main/java/com/example/lutemongame/AddLutemonActivity.java
+++ app/src/main/java/com/example/lutemongame/AddLutemonActivity.java
@@ -3,6 +3,11 @@ package com.example.lutemongame;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,11 +17,31 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class AddLutemonActivity extends AppCompatActivity {
 
+    EditText lutemonNameInput;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_add_lutemon);
+
+        //Adding lutemons
+        lutemonNameInput = findViewById(R.id.LutemonNameText);
+        RadioGroup colorGroup = findViewById(R.id.LutemonRadioGroup);
+        Button addButton = findViewById(R.id.AddLutemonButton);
+
+        //Setting listener
+        addButton.setOnClickListener(view -> {
+
+                    String lutemonName = lutemonNameInput.getText().toString();
+
+                    int selectedId = colorGroup.getCheckedRadioButtonId();
+                    RadioButton selectedButton = findViewById(selectedId);
+                    String color = selectedButton.getText().toString();
+                });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
