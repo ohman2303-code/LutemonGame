921998006d489b658b87e08c0bd773eb3964c708
lassi <lassi.lehto1@gmail.com>
2026-04-14 16:08:16 +0300

    Pieni lisäys homeFragmenttiin (Latexille tiedoksi: jatka fragmentteja)

--- app/src/main/java/com/example/lutemongame/fragments/HomeFragment.java
+++ app/src/main/java/com/example/lutemongame/fragments/HomeFragment.java
@@ -60,7 +60,8 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return view;
     }
 }
