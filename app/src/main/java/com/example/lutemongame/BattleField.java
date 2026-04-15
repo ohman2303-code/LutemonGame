5b19ddaa86e676def665a0e1454e93900abd820f
lassi <lassi.lehto1@gmail.com>
2026-04-15 15:41:28 +0300

    BattleField fragmenttia eteenpäin

--- app/src/main/java/com/example/lutemongame/BattleField.java
+++ app/src/main/java/com/example/lutemongame/BattleField.java
@@ -1,4 +1,14 @@
 package com.example.lutemongame;
 
+import java.util.ArrayList;
+
 public class BattleField extends Storage{
+    public ArrayList<String> battleLog = new ArrayList<>();
+
+    public void fight(Lutemon A, Lutemon B){
+        Boolean isBothAlive = false;
+        while(!isBothAlive){
+            ;
+        }
+    }
 }
