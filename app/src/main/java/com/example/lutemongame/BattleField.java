7d06c94c83d838f926fa645edd933844cbeeacd3
Alex Öhman <ohman2303@gmail.com>
2026-04-15 15:58:41 +0300

    Kakki on nyt singletoneja

--- app/src/main/java/com/example/lutemongame/BattleField.java
+++ app/src/main/java/com/example/lutemongame/BattleField.java
@@ -1,4 +1,17 @@
 package com.example.lutemongame;
 
 public class BattleField extends Storage{
+
+    private static BattleField battleField = null;
+
+    private BattleField(){
+        this.name = "BattleField";
+    }
+
+    public static BattleField getInstance() {
+        if (battleField == null) {
+            battleField = new BattleField();
+        }
+        return battleField;
+    }
 }
